400b6f964bace942811a9539bbf7cc7afb44b2ef
mxziino <nicoaravenafx@gmail.com>
2024-04-16 12:04:29 -0400

    add screens files

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgets_app/presentation/screens/animated/animated_Screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controlls/ui_controlls_screen.dart';
---
+++ lib/presentation/screens/ui_controlls/ui_controlls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiControllsScreen extends StatelessWidget {
+  static const name = 'ui_controlls_screen';
+  const UiControllsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
