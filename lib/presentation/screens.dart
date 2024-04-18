f40eac760b9b67a6db0c2597200953b1894d9cb7
mxziino <nicoaravenafx@gmail.com>
2024-04-18 12:52:58 -0400

    animated container done

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,4 +1,4 @@
-export 'package:widgets_app/presentation/screens/animated/animated_Screen.dart';
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
 export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
